deee11461bb5c59454c639d1f244b326f10be605
Damian Poole <damian.poole@gmail.com>
2025-10-21 11:39:22 +0100

    change lazyvim theme and update sketchy styling

--- sketchybar/plugins/app-icon.sh
+++ sketchybar/plugins/app-icon.sh
@@ -2,5 +2,5 @@
 
 if [ "$SENDER" = "front_app_switched" ]; then
   ICON=$($HOME/.config/sketchybar/plugins/icon_map.sh "$INFO")
-  sketchybar --set "$NAME" label="$ICON" icon.padding_left=10 label.font="sketchybar-app-font:Regular:15.0" label.color=0xffffffff label.drawing=on
+  sketchybar --set "$NAME" label="$ICON" label.padding_left=7 label.padding_right=7 label.font="sketchybar-app-font:Regular:14.0" label.color=0xffffffff label.drawing=on
 fi
